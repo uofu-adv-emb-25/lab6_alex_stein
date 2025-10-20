f9fd5f52de68dbae7980c9135165f33a1f9e4e76
steinwitt <steinmwitt@gmail.com>
2025-10-20 13:14:28 -0600

    working on 3 threads

---
+++ src/helper.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/cyw43_arch.h"
+
+
+#define TASK_PRIORITY     ( tskIDLE_PRIORITY + 2UL )
+
+
+void Task1(void *pvParams)
+{
+    xSemaphoreTake(xSemaphore, portMAX_DELAY);
+    vTaskDelay(pdMS_TO_TICKS(50));
+    xSemaphoreGive(xSemaphore);
+    vTaskDelete(NULL);
+}
+
+void Task2(void *pvParams)
+{
+    while(1)
+    {
+        // do some work
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+
+}
+
+void Task3(void *pvParams)
+{
+    vTaskDelay(pdMS_TO_TICKS(200));
+    xSemaphoreTake(xSemaphore, portMAX_DELAY);
+    vTaskDelay(pdMS_TO_TICKS(50));
+    xSemaphoreGive(xSemaphore);
+    vTaskDelete(NULL);
+}
+
+void Supervisor(void)
+{
+    xSemaphore = xSemaphoreCreateBinary()
+
+
+
+    BaseType_t l1 = xTaskCreate(Task1, "Low Priority Task", configMINIMAL_STACK_SIZE, NULL, 1, NULL)
+}
