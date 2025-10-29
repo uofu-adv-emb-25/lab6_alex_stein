46eb41f4e5ec74b456d71348ea3803ccec6cbd16
steinwitt <steinmwitt@gmail.com>
2025-10-28 18:41:41 -0600

    Created priority inversion in Activity 0

--- src/helper.c
+++ src/helper.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <FreeRTOS.h>
 #include <semphr.h>
 #include "task.h"
@@ -13,51 +14,83 @@
 #define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define PRIORITY_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
+SemaphoreHandle_t xSemaphore;
+
 void Task_High(void *pvParams)
 {
     vTaskDelay(pdMS_TO_TICKS(200));
-    xSemaphoreTake(xSemaphore, portMAX_DELAY);
-    vTaskDelay(pdMS_TO_TICKS(50));
-    xSemaphoreGive(xSemaphore);
+    printf("High priority taks attempting to take semaphore\n");
+
+    if(xSemaphoreTake(xSemaphore, portMAX_DELAY) == pdTRUE)    
+    {
+        printf("High priority task has taken semaphore\n");
+        xSemaphoreGive(xSemaphore);
+        printf("High priority task released semaphore\n");
+    }
+    else
+    {
+        printf("High priority task unable to take semaphore\n");
+    }
     vTaskDelete(NULL);
 }
 
 void Task_Medium(void *pvParams)
 {
+    printf("Medium task attempting to starve system\n");
     while(1)
     {
-        // starve other tasks
+        { taskYIELD(); }
+        // starve other tasks   
     }
 }
 
 void Task_Low(void *pvParams)
 {
-    xSemaphoreTake(xSemaphore, portMAX_DELAY);
-    vTaskDelay(pdMS_TO_TICKS(50));
-    xSemaphoreGive(xSemaphore);
+
+    if(xSemaphoreTake(xSemaphore, portMAX_DELAY) == pdTRUE)
+    {
+        printf("Low priority task was able to take semaphore\n");
+
+         // do something not fun for a while
+        volatile float res = 0;
+        for (int i = 0; i < 1000; i++) 
+            for (int j = 0; j < 1000; j++) 
+                for (int k = 1; k < 1000; k++) 
+                    res = sqrt((float) i);
+
+        xSemaphoreGive(xSemaphore);
+        printf("Low priority task released semaphore\n");
+    }
+    else
+    {
+        printf("Low priority task was unable to take semaphore\n");
+    }
     vTaskDelete(NULL);
 }
 
-void Task_Supervisor(void)
+void Task_Supervisor(void *pvParams)
 {
-    xSemaphore = xSemaphoreCreateBinary()
-
     xTaskCreate(Task_Low, "low_priority_thread", PRIORITY_TASK_STACK_SIZE, NULL, LOW_PRIORITY_TASK_PRIORITY, NULL);
-    vTaskDelay(500 / portTICK_PERIOD_MS);
+    vTaskDelay(pdMS_TO_TICKS(10));
     xTaskCreate(Task_Medium, "medium_priority_thread", PRIORITY_TASK_STACK_SIZE, NULL, MEDIUM_PRIORITY_TASK_PRIORITY, NULL);
+    vTaskDelay(pdMS_TO_TICKS(150));
     xTaskCreate(Task_High, "high_priority_thread", PRIORITY_TASK_STACK_SIZE, NULL, HIGH_PRIORITY_TASK_PRIORITY, NULL);
+
+    vTaskDelete(NULL);
+
 }
 
 int main() {
     stdio_init_all();
     sleep_ms(5000);
 
-    const char *rtos_name;
-    rtos_name = "FreeRTOS";
+    xSemaphore = xSemaphoreCreateBinary();
+    configASSERT(xSemaphore);
+    xSemaphoreGive(xSemaphore);
+
     TaskHandle_t task;
     xTaskCreate(Task_Supervisor, "supervisor_thread", MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
-
-    vTaskStartScheduler();  // Start FreeRTOS
+    vTaskStartScheduler(); 
 
     return 0;
 }
